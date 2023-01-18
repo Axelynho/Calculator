744af4fbb7d7ee7caba604e3030f0c2f73c84269
Axelynho <honplus45@mail.ru>
2023-01-18 18:07:52 +0400

    math / calc

---
+++ calc.js
@@ -0,0 +1,33 @@
+const OPERATIONS = {
+    sum: '+',
+    substract: '-',
+    multiply: '*',
+    division: '/'
+};
+
+function calculate({a,b,operation}) {
+    let result = null;
+
+    switch (operation) {
+        case OPERATIONS.sum:
+            
+            break;
+
+        case OPERATIONS.substract:
+
+            break;
+
+        case OPERATIONS.multiply:
+
+            break;
+
+        case OPERATIONS.division:
+
+            break;
+    
+        default:
+            break;
+    }
+
+    return result;
+}
